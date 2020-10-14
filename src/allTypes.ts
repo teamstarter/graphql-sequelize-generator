b1707ab69fae7744b43f8a328fda20d25815a2fa
Friedrich Tane <friedrich@Friedrich-Mac.local>
2020-10-14 13:59:55 +0200

    modelDeclarationType

--- src/allTypes.ts
+++ src/allTypes.ts
@@ -3,14 +3,15 @@ import {
   GraphQLScalarType,
   GraphQLNonNull,
   GraphQLObjectType,
-  GraphQLInputObjectType
+  GraphQLInputObjectType,
+  GraphQLType
 } from 'graphql'
 
 export type Action = 'list' | 'create' | 'delete' | 'update' | 'count'
 export type ActionList = Array<Action>
 
-export type Subscription = 'create' | 'delete' | 'update'
-export type SubscriptionList = Array<Subscription>
+export type Event = 'create' | 'delete' | 'update'
+export type EventList = Array<Event>
 
 export type SequelizeModel = typeof Model & {
   new (values?: object, options?: BuildOptions): any
@@ -18,7 +19,11 @@ export type SequelizeModel = typeof Model & {
 
 export type Args = any
 
+export type ExtraArg = { [key: string]: { type: GraphQLType } }
+
 export type Context = any
+
+export type Payload = any
 
 export type Types = {
   inputTypes: InputTypes
@@ -28,6 +33,8 @@ export type Types = {
 export type Info = any
 
 export type EntityProperties = any
+
+export type ListResult = any[]
 
 export type Where = any
 
@@ -65,6 +72,17 @@ export type MutationList = {
   [key: string]: CustomMutationConfiguration
 }
 
+export type CustomSubscriptionConfiguration = {
+  type: GraphQLObjectType
+  description?: string
+  args: EndpointArgs
+  resolve: CustomResolver
+}
+
+export type SubscriptionList = {
+  [key: string]: CustomMutationConfiguration
+}
+
 export type GlobalBeforeHook = (
   args: Args,
   context: Context,
@@ -76,6 +94,12 @@ export type QueryBeforeHook = (
   context: Context,
   info: Info
 ) => FindOptions
+export type ListAfterHook = (
+  result: ListResult,
+  args: Args,
+  context: Context,
+  info: Info
+) => ListResult
 export type MutationBeforeHook = (
   findOptions: FindOptions,
   args: Args,
@@ -112,6 +136,12 @@ export type DeleteAfterHook = (
   info: Info
 ) => any
 
+export type SubscriptionFilterHook = (
+  payload: Payload,
+  args: Args,
+  context: Context
+) => boolean
+
 export type graphqlSchemaDeclarationType = {
   [key: string]: modelDeclarationType
 }
@@ -119,27 +149,38 @@ export type graphqlSchemaDeclarationType = {
 export type modelDeclarationType = {
   model: SequelizeModel
   actions?: ActionList
-  subscriptions?: SubscriptionList
+  subscriptions?: EventList
   additionalMutations?: MutationList
+  additionalSubscriptions?: SubscriptionList
   excludeFromRoot?: boolean
   excludeFields?: string[]
   before?: GlobalBeforeHook[]
   list?: {
+    removeUnusedAttributes?: boolean
+    extraArg?: ExtraArg
     before?: QueryBeforeHook
+    after?: ListAfterHook
   }
   create?: {
+    extraArg?: ExtraArg
     before?: MutationBeforeHook
     after?: CreateAfterHook
+    subscriptionFilter: SubscriptionFilterHook
   }
   update?: {
+    extraArg?: ExtraArg
     before?: MutationBeforeHook
     after?: UpdateAfterHook
+    subscriptionFilter: SubscriptionFilterHook
   }
   delete?: {
+    extraArg?: ExtraArg
     before?: DeleteBeforeHook
     after?: DeleteAfterHook
+    subscriptionFilter: SubscriptionFilterHook
   }
   count?: {
+    extraArg?: ExtraArg
     before?: QueryBeforeHook
     resolver?: () => Promise<any>
   }
